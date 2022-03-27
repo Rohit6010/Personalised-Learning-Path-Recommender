100ca0e538ce47a25afd690d5e110ac6af9b8c5f
akrhythm20 <akrhythm20@gmail.com>
2022-03-27 20:35:50 +0530

    Creating all the required database tables

--- LPRS/App/models.py
+++ LPRS/App/models.py
@@ -7,3 +7,129 @@ class SignUp(models.Model):
     email = models.EmailField(max_length=50, default='')
     password = models.CharField(max_length=50, default='')
     phone = models.IntegerField(default=0)
+
+
+class Array(models.Model):
+    question = models.CharField(max_length=500)
+    option1 = models.CharField(max_length=100)
+    option2 = models.CharField(max_length=100)
+    option3 = models.CharField(max_length=100)
+    option4 = models.CharField(max_length=100)
+    answer = models.CharField(max_length=100)
+    level = models.CharField(max_length=100)
+
+    def __str__ (self):
+        return self.question
+
+
+class LinkedList(models.Model):
+    question = models.CharField(max_length=500)
+    option1 = models.CharField(max_length=100)
+    option2 = models.CharField(max_length=100)
+    option3 = models.CharField(max_length=100)
+    option4 = models.CharField(max_length=100)
+    answer = models.CharField(max_length=100)
+    level = models.CharField(max_length=100)
+
+    def __str__ (self):
+        return self.question
+    
+class Tree(models.Model):
+    question = models.CharField(max_length=500)
+    option1 = models.CharField(max_length=100)
+    option2 = models.CharField(max_length=100)
+    option3 = models.CharField(max_length=100)
+    option4 = models.CharField(max_length=100)
+    answer = models.CharField(max_length=100)
+    level = models.CharField(max_length=100)
+
+    def __str__ (self):
+        return self.question
+
+
+class Recursion(models.Model):
+    question = models.CharField(max_length=500)
+    option1 = models.CharField(max_length=100)
+    option2 = models.CharField(max_length=100)
+    option3 = models.CharField(max_length=100)
+    option4 = models.CharField(max_length=100)
+    answer = models.CharField(max_length=100)
+    level = models.CharField(max_length=100)
+
+    def __str__ (self):
+        return self.question
+
+class DP(models.Model):
+    question = models.CharField(max_length=500)
+    option1 = models.CharField(max_length=100)
+    option2 = models.CharField(max_length=100)
+    option3 = models.CharField(max_length=100)
+    option4 = models.CharField(max_length=100)
+    answer = models.CharField(max_length=100)
+    level = models.CharField(max_length=100)
+
+    def __str__ (self):
+        return self.question
+
+
+class Graph(models.Model):
+    question = models.CharField(max_length=500)
+    option1 = models.CharField(max_length=100)
+    option2 = models.CharField(max_length=100)
+    option3 = models.CharField(max_length=100)
+    option4 = models.CharField(max_length=100)
+    answer = models.CharField(max_length=100)
+    level = models.CharField(max_length=100)
+
+    def __str__ (self):
+        return self.question
+    
+
+class Stack(models.Model):
+    question = models.CharField(max_length=500)
+    option1 = models.CharField(max_length=100)
+    option2 = models.CharField(max_length=100)
+    option3 = models.CharField(max_length=100)
+    option4 = models.CharField(max_length=100)
+    answer = models.CharField(max_length=100)
+    level = models.CharField(max_length=100)
+
+    def __str__ (self):
+        return self.question
+
+class Queue(models.Model):
+    question = models.CharField(max_length=500)
+    option1 = models.CharField(max_length=100)
+    option2 = models.CharField(max_length=100)
+    option3 = models.CharField(max_length=100)
+    option4 = models.CharField(max_length=100)
+    answer = models.CharField(max_length=100)
+    level = models.CharField(max_length=100)
+
+    def __str__ (self):
+        return self.question
+
+class String(models.Model):
+    question = models.CharField(max_length=500)
+    option1 = models.CharField(max_length=100)
+    option2 = models.CharField(max_length=100)
+    option3 = models.CharField(max_length=100)
+    option4 = models.CharField(max_length=100)
+    answer = models.CharField(max_length=100)
+    level = models.CharField(max_length=100)
+
+    def __str__ (self):
+        return self.question
+
+
+class Heap(models.Model):
+    question = models.CharField(max_length=500)
+    option1 = models.CharField(max_length=100)
+    option2 = models.CharField(max_length=100)
+    option3 = models.CharField(max_length=100)
+    option4 = models.CharField(max_length=100)
+    answer = models.CharField(max_length=100)
+    level = models.CharField(max_length=100)
+
+    def __str__ (self):
+        return self.question
--- LPRS/App/urls.py
+++ LPRS/App/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('signup/', views.handleSignup),
     path('login/', views.handleLogin),
+    path('test/', views.test, name="test")
 ]
