a1ce9445e10a2f940fe8232949786cda07410ef8
akrhythm20 <akrhythm20@gmail.com>
2022-04-25 09:36:21 +0530

    adding data

--- LPRS/App/models.py
+++ LPRS/App/models.py
@@ -8,7 +8,6 @@ class SignUp(models.Model):
     password = models.CharField(max_length=50, default='')
     phone = models.IntegerField(default=0)
 
-
 class Array(models.Model):
     question = models.CharField(max_length=500)
     option1 = models.CharField(max_length=100)
@@ -20,7 +19,6 @@ class Array(models.Model):
 
     def __str__ (self):
         return '(' + str(self.id) + ') ' + self.level
-
 
 class LinkedList(models.Model):
     question = models.CharField(max_length=500)
@@ -46,7 +44,6 @@ class Tree(models.Model):
     def __str__ (self):
         return self.question
 
-
 class Recursion(models.Model):
     question = models.CharField(max_length=500)
     option1 = models.CharField(max_length=100)
@@ -71,7 +68,6 @@ class DP(models.Model):
     def __str__ (self):
         return self.question
 
-
 class Graph(models.Model):
     question = models.CharField(max_length=500)
     option1 = models.CharField(max_length=100)
@@ -84,7 +80,6 @@ class Graph(models.Model):
     def __str__ (self):
         return self.question
     
-
 class Stack(models.Model):
     question = models.CharField(max_length=500)
     option1 = models.CharField(max_length=100)
@@ -121,7 +116,6 @@ class String(models.Model):
     def __str__ (self):
         return self.question
 
-
 class Heap(models.Model):
     question = models.CharField(max_length=500)
     option1 = models.CharField(max_length=100)
@@ -134,22 +128,55 @@ class Heap(models.Model):
     def __str__ (self):
         return self.question
 
-
 class Student(models.Model):
-    user = models.IntegerField(primary_key=True)
+    userid = models.IntegerField(primary_key=True)
     username = models.CharField(max_length=100)
     name = models.CharField(max_length=50, blank=True, null=True)
     phone = models.CharField(max_length=13, blank=True, null=True)
-    scoreBeg = models.IntegerField(default=0)
-    scoreImd = models.IntegerField(default=0)
-    scoreExp = models.IntegerField(default=0) 
     textual_trait = models.IntegerField(default=0)
     visual_trait = models.IntegerField(default=0)
+    curr_topic = models.CharField(max_length=50, blank=True, null=True)
+    language = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__ (self):
         return self.username
 
+class AssessDetails(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    topic = models.CharField(max_length=30)
+    scoreBeg = models.IntegerField(default=0)
+    scoreImd = models.IntegerField(default=0)
+    scoreExp = models.IntegerField(default=0) 
+    level = models.CharField(max_length=20, blank=True, null=True)
 
+    def __str__ (self):
+        return self.student.username + "_" + self.topic 
+
+class Content(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    subtopic = models.CharField(max_length=30)
+
+    text1 = models.CharField(max_length=5000,blank=True, null=True)
+    text2 = models.CharField(max_length=5000,blank=True, null=True)
+    text3 = models.CharField(max_length=5000,blank=True, null=True)
+    text4 = models.CharField(max_length=5000,blank=True, null=True)
+
+    video1 = models.CharField(max_length=5000, blank=True, null=True)
+    video2 = models.CharField(max_length=5000, blank=True, null=True)
+    video3 = models.CharField(max_length=5000, blank=True, null=True)
+    video4 = models.CharField(max_length=5000, blank=True, null=True)
+   
+    def __str__ (self):
+        return self.subtopic
+
+class TopicId(models.Model):
+     id = models.IntegerField(default=0, primary_key=True)
+     topic = models.CharField(max_length=50, blank=True, null=True)
+     
+
+     def __str__ (self):
+        return self.topic
+  
 class Cognitive(models.Model):
     question = models.CharField(max_length=1000)
     option_text = models.CharField(max_length=100)
@@ -158,14 +185,11 @@ class Cognitive(models.Model):
     def __str__ (self):
         return self.question
 
-
-
 class Practice(models.Model):
     topic = models.CharField(max_length=50)
     question = models.CharField(max_length=500)
     link = models.CharField(max_length=500)
     level = models.CharField(max_length=50)
-    # subTopic = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__ (self):
         return "("+self.topic+") ("+self.level+") "+ self.question
